216795c8ecc44a99091d66be4a17555b72927c7b
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2025-07-12 14:38:32 +0300

    CategoryApiController yazıldı

--- app/Http/Controllers/Api/CategoryApiController.php
+++ app/Http/Controllers/Api/CategoryApiController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class CategoryApiController extends Controller
 {
-    //
+
+    public function store(Request $request){
+      $request->validate([
+        "ad"=>"required"
+      ]);
+      Kategori::create($request->only("ad"));
+      return response()->json(["message"=>"basarılı bir sekilde kategori eklendi"]);
+
+    }
+    public function update(Request $request,$id){
+    $request->validate(["ad"=>"required"]);
+      Kategori::findOrFail($id)->update($request->only("ad"));
+return response()->json(["message"=>"basarılı bir sekilde guncellendi"]);
+    }
+    public function destroy(Request $request,$id){
+    
+        Kategori::findOrFail($id)->delete();
+return response()->json(["message"=>"basarılı bir sekilde silinid"]);
+    }
 }
